a37d5b97bd22270de17d3c150bb27edd4744c1f7
wvilchez04 <walter_vilu@hotmail.com>
2021-07-21 12:48:30 -0500

    Create reto2.py

---
+++ seison1retos/reto2.py
@@ -0,0 +1,23 @@
+import datetime
+import calendar
+personas = {}
+nombres=[]
+dnis=[]
+fechas_nac=[]
+
+cantidad = int(input("Introduce la cantidad de alumnos que vamos a guradar:"))
+for num in range(cantidad):
+    nombre = input("Nombre de la persona:")
+    while nombre in personas:
+        nombres.append(nombre)
+        print("Persona ya existe.")
+        nombre = input("Nombre de la persona:")
+    dni = int(input("Ingrese su dni:"))
+    while dni > 0 :
+        dnis.append(dni)
+        dni = int(input("Ingrese dni correctamente:"))
+    fecha_nac = datetime.datetime.strptime(input("ingrese su fecha de nacimiento (AAAA/MM/DD): "),"%Y/%m/%d")
+    fechas_nac.append(calendar.timegm(fecha_nac.timetuple()))
+    
+for nombres, dnis in personas.items():
+    print("%s ha sacado de nota media %f" % (nombre,sum(dnis)/len(dnis)))
